04f3eabed1b42858469cd29a519b8ad24dbf0f42
BuildTools <unconfigured@null.spigotmc.org>
2023-09-18 11:51:40 -0400

    added detection for reserved words. intifiers, literals, consts not included.

--- src/edu/ufl/cise/cop4020fa23/Lexer.java
+++ src/edu/ufl/cise/cop4020fa23/Lexer.java
@@ -35,11 +35,171 @@ public class Lexer implements ILexer {
 
 		while(pos != input.length())
 		{
-			if(!((input.charAt(pos) == ' ') || (input.charAt(pos) == '\n') || (input.charAt(pos) == '\r') || (input.charAt(pos) == '\t')))
+			if(!((input.charAt(pos) == ' ') || (input.charAt(pos) == '\n') || (input.charAt(pos) == '\r') || (input.charAt(pos) == '\t') || (input.charAt(pos) == '#')))
 			{
 				int startPos = pos;
 
-				if(input.substring(startPos, startPos+2).equals("=="))
+				if(input.substring(startPos, startPos+5).equals("image"))
+				{
+					if((input.charAt(startPos+6) == ' ') || (input.charAt(startPos+6) == '\n') || (input.charAt(startPos+6) == '\r') || (input.charAt(startPos+6) == '\t'))
+					{
+						Token temp = new Token(Kind.RES_image, startPos, 5, input.substring(startPos, startPos+5).toCharArray(), new SourceLocation(line, col));
+						col += 5;
+						pos += 5;
+						tokens.add(temp);
+					}
+				}
+				else if(input.substring(startPos, startPos+5).equals("pixel"))
+				{
+					if((input.charAt(startPos+6) == ' ') || (input.charAt(startPos+6) == '\n') || (input.charAt(startPos+6) == '\r') || (input.charAt(startPos+6) == '\t'))
+					{
+						Token temp = new Token(Kind.RES_pixel, startPos, 5, input.substring(startPos, startPos+5).toCharArray(), new SourceLocation(line, col));
+						col += 5;
+						pos += 5;
+						tokens.add(temp);
+					}
+				}
+				else if(input.substring(startPos, startPos+3).equals("int"))
+				{
+					if((input.charAt(startPos+4) == ' ') || (input.charAt(startPos+4) == '\n') || (input.charAt(startPos+4) == '\r') || (input.charAt(startPos+4) == '\t'))
+					{
+						Token temp = new Token(Kind.RES_int, startPos, 3, input.substring(startPos, startPos+3).toCharArray(), new SourceLocation(line, col));
+						col += 3;
+						pos += 3;
+						tokens.add(temp);
+					}
+				}
+				else if(input.substring(startPos, startPos+6).equals("string"))
+				{
+					if((input.charAt(startPos+7) == ' ') || (input.charAt(startPos+7) == '\n') || (input.charAt(startPos+7) == '\r') || (input.charAt(startPos+7) == '\t'))
+					{
+						Token temp = new Token(Kind.RES_string, startPos, 6, input.substring(startPos, startPos+6).toCharArray(), new SourceLocation(line, col));
+						col += 6;
+						pos += 6;
+						tokens.add(temp);
+					}
+				}
+				else if(input.substring(startPos, startPos+4).equals("void"))
+				{
+					if((input.charAt(startPos+5) == ' ') || (input.charAt(startPos+5) == '\n') || (input.charAt(startPos+5) == '\r') || (input.charAt(startPos+5) == '\t'))
+					{
+						Token temp = new Token(Kind.RES_void, startPos, 4, input.substring(startPos, startPos+4).toCharArray(), new SourceLocation(line, col));
+						col += 4;
+						pos += 4;
+						tokens.add(temp);
+					}
+				}
+				else if(input.substring(startPos, startPos+7).equals("boolean"))
+				{
+					if((input.charAt(startPos+8) == ' ') || (input.charAt(startPos+8) == '\n') || (input.charAt(startPos+8) == '\r') || (input.charAt(startPos+8) == '\t'))
+					{
+						Token temp = new Token(Kind.RES_boolean, startPos, 7, input.substring(startPos, startPos+7).toCharArray(), new SourceLocation(line, col));
+						col += 7;
+						pos += 7;
+						tokens.add(temp);
+					}
+				}
+				else if(input.substring(startPos, startPos+5).equals("write"))
+				{
+					if((input.charAt(startPos+6) == ' ') || (input.charAt(startPos+6) == '\n') || (input.charAt(startPos+6) == '\r') || (input.charAt(startPos+6) == '\t'))
+					{
+						Token temp = new Token(Kind.RES_write, startPos, 5, input.substring(startPos, startPos+5).toCharArray(), new SourceLocation(line, col));
+						col += 5;
+						pos += 5;
+						tokens.add(temp);
+					}
+				}
+				else if(input.substring(startPos, startPos+6).equals("height"))
+				{
+					if((input.charAt(startPos+7) == ' ') || (input.charAt(startPos+7) == '\n') || (input.charAt(startPos+7) == '\r') || (input.charAt(startPos+7) == '\t'))
+					{
+						Token temp = new Token(Kind.RES_height, startPos, 6, input.substring(startPos, startPos+6).toCharArray(), new SourceLocation(line, col));
+						col += 6;
+						pos += 6;
+						tokens.add(temp);
+					}
+				}
+				else if(input.substring(startPos, startPos+5).equals("width"))
+				{
+					if((input.charAt(startPos+6) == ' ') || (input.charAt(startPos+6) == '\n') || (input.charAt(startPos+6) == '\r') || (input.charAt(startPos+6) == '\t'))
+					{
+						Token temp = new Token(Kind.RES_width, startPos, 5, input.substring(startPos, startPos+5).toCharArray(), new SourceLocation(line, col));
+						col += 5;
+						pos += 5;
+						tokens.add(temp);
+					}
+				}
+				else if(input.substring(startPos, startPos+2).equals("if"))
+				{
+					if((input.charAt(startPos+3) == ' ') || (input.charAt(startPos+3) == '\n') || (input.charAt(startPos+3) == '\r') || (input.charAt(startPos+3) == '\t'))
+					{
+						Token temp = new Token(Kind.RES_if, startPos, 2, input.substring(startPos, startPos+2).toCharArray(), new SourceLocation(line, col));
+						col += 2;
+						pos += 2;
+						tokens.add(temp);
+					}
+				}
+				else if(input.substring(startPos, startPos+2).equals("fi"))
+				{
+					if((input.charAt(startPos+3) == ' ') || (input.charAt(startPos+3) == '\n') || (input.charAt(startPos+3) == '\r') || (input.charAt(startPos+3) == '\t'))
+					{
+						Token temp = new Token(Kind.RES_fi, startPos, 2, input.substring(startPos, startPos+2).toCharArray(), new SourceLocation(line, col));
+						col += 2;
+						pos += 2;
+						tokens.add(temp);
+					}
+				}
+				else if(input.substring(startPos, startPos+2).equals("do"))
+				{
+					if((input.charAt(startPos+3) == ' ') || (input.charAt(startPos+3) == '\n') || (input.charAt(startPos+3) == '\r') || (input.charAt(startPos+3) == '\t'))
+					{
+						Token temp = new Token(Kind.RES_do, startPos, 2, input.substring(startPos, startPos+2).toCharArray(), new SourceLocation(line, col));
+						col += 2;
+						pos += 2;
+						tokens.add(temp);
+					}
+				}
+				else if(input.substring(startPos, startPos+2).equals("od"))
+				{
+					if((input.charAt(startPos+3) == ' ') || (input.charAt(startPos+3) == '\n') || (input.charAt(startPos+3) == '\r') || (input.charAt(startPos+3) == '\t'))
+					{
+						Token temp = new Token(Kind.RES_od, startPos, 2, input.substring(startPos, startPos+2).toCharArray(), new SourceLocation(line, col));
+						col += 2;
+						pos += 2;
+						tokens.add(temp);
+					}
+				}
+				else if(input.substring(startPos, startPos+3).equals("red"))
+				{
+					if((input.charAt(startPos+4) == ' ') || (input.charAt(startPos+4) == '\n') || (input.charAt(startPos+4) == '\r') || (input.charAt(startPos+4) == '\t'))
+					{
+						Token temp = new Token(Kind.RES_red, startPos, 3, input.substring(startPos, startPos+3).toCharArray(), new SourceLocation(line, col));
+						col += 3;
+						pos += 3;
+						tokens.add(temp);
+					}
+				}
+				else if(input.substring(startPos, startPos+5).equals("green"))
+				{
+					if((input.charAt(startPos+6) == ' ') || (input.charAt(startPos+6) == '\n') || (input.charAt(startPos+6) == '\r') || (input.charAt(startPos+6) == '\t'))
+					{
+						Token temp = new Token(Kind.RES_green, startPos, 5, input.substring(startPos, startPos+5).toCharArray(), new SourceLocation(line, col));
+						col += 5;
+						pos += 5;
+						tokens.add(temp);
+					}
+				}
+				else if(input.substring(startPos, startPos+4).equals("blue"))
+				{
+					if((input.charAt(startPos+5) == ' ') || (input.charAt(startPos+5) == '\n') || (input.charAt(startPos+5) == '\r') || (input.charAt(startPos+5) == '\t'))
+					{
+						Token temp = new Token(Kind.RES_blue, startPos, 4, input.substring(startPos, startPos+4).toCharArray(), new SourceLocation(line, col));
+						col += 4;
+						pos += 4;
+						tokens.add(temp);
+					}
+				}
+				else if(input.substring(startPos, startPos+2).equals("=="))
 				{
 					Token temp = new Token(Kind.EQ, startPos, 2, input.substring(startPos, startPos+2).toCharArray(), new SourceLocation(line, col));
 					col += 2;
